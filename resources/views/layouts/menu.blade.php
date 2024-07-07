ba34e2f2c10e94fa9eeb42fe133a4bf0d657ba08
Marwan1998 <Dragonkingofhell@gmail.com>
2024-07-07 21:03:06 +0200

    meun icon

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,7 +1,7 @@
 <li class="nav-item">
     <a href="{{ route('users.index') }}"
-       class="nav-link {{ Request::is('users*') ? 'active' : '' }}">
-        <p>@lang('models/users.plural')</p>
+    class="nav-link {{ Request::is('users*') ? 'active' : '' }}">
+    <i class="fa fa-users nav-icon"></i>
+    <p>@lang('models/users.plural')</p>
     </a>
 </li>
-

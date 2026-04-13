3aefb00e1d2071a782a3a448f9e9948967244eed
Marwan1998 <dragonkingofhell@gmail.com>
2026-04-13 16:38:32 +0200

    refactor class name

--- app/Services/RunExtraction/Workover/AGOCOExtractionDumpWorkover.php
+++ app/Services/RunExtraction/Workover/AGOCOExtractionDumpWorkover.php
@@ -3,13 +3,13 @@
 namespace App\Services\RunExtraction\Workover;
 
 use App\Services\Pdf\AGOCOWellExtractorWorkover;
-use App\Services\RunExtraction\ExtractionDumpWorkover_NEW;
+use App\Services\RunExtraction\ExtractionDumpWorkover;
 
 use PhpOffice\PhpSpreadsheet\Shared\Date as ExcelDate;
 use Carbon\Carbon;
 
 
-class AGOCOExtractionDumpWorkover extends ExtractionDumpWorkover_NEW
+class AGOCOExtractionDumpWorkover extends ExtractionDumpWorkover
 {
     public function __construct()
     {

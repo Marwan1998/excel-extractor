0642f89f1b6616c6abb42d2d9e8162cd0ff73b51
Marwan1998 <dragonkingofhell@gmail.com>
2026-04-19 12:28:00 +0200

    added extractDateFromFileName helper, refactoring

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -45,6 +45,24 @@ if (!function_exists('logd')) {
     }
 }
 
+if (!function_exists('logData')) {
+    function logData($fileNames, $extractor)
+    {
+        $allData = [];
+        foreach ($fileNames as $value) {
+            $filePath = storage_path($value['name']);
+            $extractor = new $extractor();
+            $data = $extractor->extract($filePath);
+
+            array_push($allData, [count($data) => $data]);
+            
+            \Log::debug('Data', [$value['name'] => count($data)]);
+        }
+
+        return $allData;   
+    }
+}
+
 if (!function_exists('cleanNumericValue')) {
     function cleanNumericValue($value)
     {
@@ -101,5 +119,42 @@ if (!function_exists('getExcelDateFormat')) {
 
         // Convert to Excel serial number
         return ExcelDate::PHPToExcel($date);
+    }
+}
+
+if (!function_exists('extractDateFromFileName')) {
+    function extractDateFromFileName(string $filename)
+    {
+        // Remove extension to avoid confusion with numbers in .xlsx or .docx
+        $nameWithoutExt = pathinfo($filename, PATHINFO_FILENAME);
+
+        // 1. Try to catch standard numeric patterns: DD-MM-YYYY, DD.MM.YYYY, DD/MM/YYYY, or D-M-YYYY
+        // Matches: 01-03-2026, 1-4-2026, 19.03.2026
+        if (preg_match('/(\d{1,2})[\-\.\/](\d{1,2})[\-\.\/](\d{4})/', $nameWithoutExt, $matches)) {
+            try {
+                // Note: In your list, these look like Day-Month-Year (e.g., 19.03)
+                return Carbon::createFromFormat('d-m-Y', "{$matches[1]}-{$matches[2]}-{$matches[3]}")
+                            ->format('m/d/Y');
+            } catch (\Exception $e) {
+                // Fallback if Day/Month were swapped in the filename
+            }
+        }
+
+        // 2. Try to catch Month Name patterns: "MAR-14-2026", "Jan. 01, 2026", "Mar.02.2026"
+        // Regex looks for: (Month Name) (Separator) (Day) (Separator) (Year)
+        $monthRegex = '/(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)[a-z]*[\s\.\-\_]*(\d{1,2})[\s\.\,\-\_]*(\d{4})/i';
+        
+        if (preg_match($monthRegex, $nameWithoutExt, $matches)) {
+            try {
+                $month = $matches[1];
+                $day = $matches[2];
+                $year = $matches[3];
+                return Carbon::parse("{$month} {$day} {$year}")->format('m/d/Y');
+            } catch (\Exception $e) {
+                // Log error or ignore
+            }
+        }
+
+        return null; // Or return a default value if no date is found
     }
 }
